69d8077762779bac9e0891fd995d3b2b47ea9423
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-04-27 17:04:36 +0100

    Accessories component

---
+++ src/components/Accessories.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="accessories">
+        <div class="bodyimg"><img src="../assets/img/newbodyimg.png" alt=""></div>
+        <div class="mainbody">
+            <div class="electronicsframe">
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+                <div class="frame">
+                    <div class="frametext">Electronics</div>
+                    <div class="framebox"></div>
+                    <div class="framelink"><a href="">See more</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AccessoriesSection",
+}
+</script>
+
+<style scoped>
+.accessories{
+    width: inherit;
+    height: fit-content;
+    min-height: 200px;
+
+}
+.bodyimg{
+    width: inherit;
+    height: 400px;
+    overflow: hidden;
+}
+.bodyimg img{
+    width: 100%;
+    height: 100%;
+}
+.mainbody{
+    width: inherit;
+    height: fit-content;
+    background: #E3E6E6;
+}
+.electronicsframe{
+    width: inherit;
+    height: fit-content;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0;
+    padding-top: 25px;
+    padding-bottom: 20px;
+    padding-right: 10px;
+    padding-left: 10px;
+    align-content: space-evenly;
+    justify-content: space-evenly;
+}
+.electronicsframe .frame{
+    width: 292px;
+    height: 400px;
+    background: #FFFFFF;
+    margin: auto;
+    padding: auto;
+    margin-bottom: 18px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+.electronicsframe .frame .frametext, .electronicsframe .frame .framelink{
+    margin-left: 30px;
+}
+.electronicsframe .frame .frametext{
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
+    color: #0f1111;
+}
+.electronicsframe .frame .framebox{
+    width: 230px;
+    height: 300px;
+    background: #d9d9d9;
+    align-self: center;
+}
+.electronicsframe .frame .framelink{
+    text-decoration: none;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 22px;
+    color: #68abb7;
+}
+</style>
