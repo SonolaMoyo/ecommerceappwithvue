4fc15857938723f1915616ff5d85bb587fb5afbf
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-05-15 12:05:28 +0100

    update on App vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
     <TopBar />
-    <AccessoriesSection />
+    <!-- <AccessoriesSection />
     
     <ProductPage />
+
+    <MainProductPage />
+
+    <SignUp />
+
+    <LogIn /> -->
+
+    <router-view></router-view>
+
     <FooterPage />
   </div>
 </template>
@@ -11,16 +20,22 @@
 <script>
 import TopBar from './components/Topbar.vue'
 import FooterPage from './components/Footer.vue'
-import AccessoriesSection from './components/Accessories.vue'
-import ProductPage from './components/ProductPage.vue'
+// import AccessoriesSection from './components/Accessories.vue'
+// import ProductPage from './components/ProductPage.vue'
+// import MainProductPage from './components/MainProductPage.vue'
+// import SignUp from './components/Signup.vue'
+// import LogIn from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
     TopBar,
     FooterPage,
-    AccessoriesSection,
-    ProductPage
+    // AccessoriesSection,
+    // ProductPage,
+    // MainProductPage,
+    // SignUp,
+    // LogIn
   }
 }
 </script>
