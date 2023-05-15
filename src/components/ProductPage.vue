78ff8648ff9106fd0fd291a8084d9453432dddd2
SonolaMoyo <sonolamoyosoluwalorun@gmail.com>
2023-05-15 12:04:34 +0100

    update on ProductPage vue

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,10 +1,36 @@
-class<template>
+class
+<template>
   <div class="productpage">
     <div id="cover">
-      <h2 id="title">Beauty and Personla Care</h2>
-      <p id="desc">Shop makeup, skin care, hair care, fragrances and more</p>
+      <h2 id="title">Results </h2>
+      <p id="desc">Price and other details may vary based on product size and color.</p>
       <div class="products">
-        <div class="product">
+        <div
+          class="product"
+          v-for="product in products"
+          v-bind:key="product._id"
+          @click="openProduct(product._id)"
+        >
+          <div
+            class="productimg"
+            :style="{ backgroundImage: `url(${product.imgLink})` }"
+          ></div>
+          <p class="productdesc">
+            {{ product.productName }}
+          </p>
+          <p class="productquantity">{{ product.quantity }}</p>
+          <div class="productrating">
+            <p>{{ product.rating }}</p>
+            <div><img src="../assets/img/star-rate.png" alt="" /></div>
+            <p>{{ product.rateAmount }}</p>
+          </div>
+          <p class="productprice">{{ product.price }}</p>
+          <p class="productdelivery">
+            Delivery Fri, {{ product.delivery }} <br />
+            Or fastest delivery {{ product.fastestDelivery }}
+          </p>
+        </div>
+        <!-- <div class="product">
           <div class="productimg"></div>
           <p class="productdesc">
             Mielle Organics RoseMary Mint Scalp & Hair Strengthening Oil With
@@ -18,7 +44,7 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
         </div>
@@ -36,7 +62,7 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
         </div>
@@ -54,7 +80,7 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
         </div>
@@ -72,7 +98,7 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
         </div>
@@ -90,7 +116,7 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
         </div>
@@ -108,7 +134,7 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
         </div>
@@ -126,7 +152,7 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
         </div>
@@ -144,36 +170,47 @@ class<template>
           </div>
           <p class="productprice">$9.00</p>
           <p class="productdelivery">
-            Delivery Fri, May 5 <br>
+            Delivery Fri, May 5 <br />
             Or fastest delivery Wed Apr 19
           </p>
-        </div>
-        <div class="product">
-          <div class="productimg"></div>
-          <p class="productdesc">
-            Mielle Organics RoseMary Mint Scalp & Hair Strengthening Oil With
-            Biotion & Essential Oils, Nourishing Treatment
-          </p>
-          <p class="productquantity">2 Fl Oz (Pack of 1)</p>
-          <div class="productrating">
-            <p>4.6</p>
-            <div><img src="../assets/img/star-rate.png" alt="" /></div>
-            <p>(32,216)</p>
-          </div>
-          <p class="productprice">$9.00</p>
-          <p class="productdelivery">
-            Delivery Fri, May 5 <br>
-            Or fastest delivery Wed Apr 19
-          </p>
-        </div>
-      </div>
+        </div> -->
+      </div> 
     </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
 export default {
   name: "ProductPage",
+  data() {
+    return { products: undefined };
+  },
+  methods: {
+    openProduct(id) {
+      this.$router.push(`/products/category/${id}`);
+    },
+  },
+  mounted() {
+    const category = this.$route.params.category;
+
+    if (category) {
+      Vue.axios
+        .get(`http://127.0.0.1:3000/product/productByCategory/${category}`)
+        .then((resp) => {
+          this.products = resp.data.products;
+          //console.log(this.$route.params.category);
+        });
+    } else {
+      Vue.axios.get("http://127.0.0.1:3000/product").then((resp) => {
+        this.products = resp.data.products;
+        //console.log(this.$route.params.category);
+      });
+    }
+  },
 };
 </script>
 
@@ -224,40 +261,40 @@ export default {
   margin-bottom: 18px;
 }
 .productimg {
-  width: 100%;
+  width: 95%;
   height: 290px;
   background: #d9d9d9;
 }
-.productdesc{
-    font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 19px;
+.productdesc {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
 }
-.productquantity{
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 19px;
+.productquantity {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
 }
-.productrating{
-    display: flex;
-    align-content: center;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 19px;
+.productrating {
+  display: flex;
+  align-content: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
 }
-.productprice{
-    font-style: normal;
-    font-weight: 400;
-    font-size: 28px;
-    line-height: 34px;
+.productprice {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 34px;
 }
-.productdelivery{
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 17px;
+.productdelivery {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
 }
 </style>
